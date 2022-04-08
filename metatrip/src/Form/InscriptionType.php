a047c2a910a466fbb7c636ecf765885cbba18893
fareslam <fares.lamloum@esprit.tn>
2022-04-08 06:37:36 +0100

    init project

--- metatrip/src/Form/InscriptionType.php
+++ metatrip/src/Form/InscriptionType.php
@@ -2,29 +2,32 @@
 
 namespace App\Form;
 
-use App\Entity\VoyageOrganise;
+use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class VoyageOrganiseType extends AbstractType
+class InscriptionType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('prixBillet')
-            ->add('airline')
-            ->add('nbNuitees')
-            ->add('etatvoyage')
-            ->add('nbplaces')
-            ->add('idv')
+            ->add('cin')
+            ->add('nom')
+            ->add('prenom')
+            ->add('tel')
+            ->add('email')
+            ->add('password')
+            ->add('image')
+          
+            ->add('datenaissance')
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => VoyageOrganise::class,
+            'data_class' => User::class,
         ]);
     }
 }
