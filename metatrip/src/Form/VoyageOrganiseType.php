c45cfa769aef14ca44ff1ea670c43eccd1c5d931
fareslam <fares.lamloum@esprit.tn>
2022-04-10 03:17:32 +0100

    new setup p

---
+++ metatrip/src/Form/VoyageOrganiseType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\VoyageOrganise;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class VoyageOrganiseType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('prixBillet')
+            ->add('airline')
+            ->add('nbNuitees')
+            ->add('nbplaces')
+            ->add('etatvoyage')
+            ->add('idv')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => VoyageOrganise::class,
+        ]);
+    }
+}
